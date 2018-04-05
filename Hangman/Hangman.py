5b24688d009984170fde63823b7318829615d3f7
Mateusz Mazurczak <kaspazza@gmail.com>
2018-04-05 12:35:58 +0200

    Some updates
    Some new features

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,76 +1,98 @@
 import random
-
+from datetime import datetime
+start_time = datetime.now()
 dic = {}
-
-with open("countriesandcapitals.txt", "r") as capitals:
-    for line in capitals:
-        (key, val) = line.split(" | ")
-        dic[key] = val
-
-i = 0
-points = 5
-password = random.choice(list(dic.values()))
-password = password.upper()
-password = password.replace('\n', '')
-hidden_password = []
-hidden_password2 = []
-
-print(password)
-y = 0
-
-for letter in password:
-    if letter == " ":
-        hidden_password.append(" ")
-    else:
-        hidden_password.append("_")
-    y += 1
-
-print("".join(hidden_password))
-
+print("Welcome to hangman, his life depends on you!")
+print("1.Start game")
+print("2.How to play")
+print("3.Leaderboard")
+print("4.Exit")
+num = input("what you want to do?")
 while True:
-    if points > 0 and ("".join(hidden_password)) != password:        
-        letter_or_word = input('Type: "l" if You want to guess letter or "w" if You want a word.')
-        hidden_password2 = hidden_password[:]
+    if num == "1":
+        with open("countriesandcapitals.txt", "r") as capitals:
+            for line in capitals:
+                (key, val) = line.split(" | ")
+                dic[key] = val
+        i = 0
+        points = 5
+        password = random.choice(list(dic.values()))
+        password = password.upper()
+        password = password.replace('\n', '')
+        hidden_password = []
+        hidden_password2 = []
 
-        if letter_or_word == "l":
-            letter_guess = input("What's the letter then? ").upper()
-                    
-            for l in password:
+        print(password)
+        y = 0
 
-                if l == letter_guess:
-                    x = 0
-                    for letter in password:
-
-                        if letter == " ":
-                            hidden_password[x] = " "
-                        elif letter == letter_guess:
-                            hidden_password[x] = letter_guess                      
-                        elif letter == "_":
-                            hidden_password[x] = "_"
-                        x += 1
-            print("".join(hidden_password))
-
-            if hidden_password2 == hidden_password:
-                points -= 1
-                print("Boo! You have -1 points You bastard!")            
-                  
-        elif letter_or_word == "w":    
-            print(password)
-            word_guess = input("What's the word then? ").upper()
-            print(word_guess)
-
-            if word_guess == password:
-                print("Great! You won the game!")
-                break            
+        for letter in password:
+            if letter == " ":
+                hidden_password.append(" ")
             else:
-                points -= 2
-                print("Boo! You have -2 points You bastard!")
-        else:
-            print("Type a proper letter!")
+                hidden_password.append("_")
+            y += 1
 
-    elif points <= 0:
-        print("You lost!")
+        print("".join(hidden_password))
+
+        while True:
+            if points > 0 and ("".join(hidden_password)) != password:
+                letter_or_word = input('Type: "l" if You want to guess letter or "w" if You want a word.')
+                hidden_password2 = hidden_password[:]
+
+                if letter_or_word == "l":
+                    letter_guess = input("What's the letter then? ").upper()
+
+                    for l in password:
+
+                        if l == letter_guess:
+                            x = 0
+                            for letter in password:
+
+                                if letter == " ":
+                                    hidden_password[x] = " "
+                                elif letter == letter_guess:
+                                    hidden_password[x] = letter_guess
+                                elif letter == "_":
+                                    hidden_password[x] = "_"
+                                x += 1
+                    print("".join(hidden_password))
+
+                    if hidden_password2 == hidden_password:
+                        points -= 1
+                        print("Boo! You have -1 points You bastard!")
+
+                elif letter_or_word == "w":
+                    print(password)
+                    word_guess = input("What's the word then? ").upper()
+                    print(word_guess)
+
+                    if word_guess == password:
+                        print("Great! You won the game!")
+                        break
+                    else:
+                        points -= 2
+                        print("Boo! You have -2 points You bastard!")
+                else:
+                    print("Type a proper letter!")
+
+            elif points <= 0:
+                print("You lost!")
+                break
+            elif ("".join(hidden_password)) == password:
+                print("Great! You won the game!")
+                break
         break
-    elif ("".join(hidden_password)) == password:
-        print("Great! You won the game!")
+    elif num == "2":
+       print("you just do this and that")
+       break
+
+    elif num == "3":
+        #plik z tym
         break
+
+    elif num == "4":
+        print("BYE BYE")
+        break
+
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
